41f1f7c7514d94026e1dcb977a5a4472fd1e2b2c
Arbab <arifarbab21@gmail.com>
2025-06-05 16:18:29 -0400

    progress through DLL

---
+++ Section 18 Doubly Linked List/192 Traverse Method Doubly Linked List.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+        
+    def __str__(self):
+        return f'{self.value}'
+        
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+    def __str__(self):
+        result = ''
+        curr = self.head
+        
+        while curr:
+            result += f'{curr.value}'
+            if curr.next:
+                result += ' <-> '
+            curr = curr.next
+        return result
+    
+    def append(self, value):
+        newNode = Node(value)
+        if self.head == None:
+            self.head = newNode
+            self.tail = newNode
+
+        else:
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+        
+        self.length += 1
+    
+    def prepend(self,value): #time complex O(1)
+        newNode = Node(value)
+        
+        if self.head == None:
+            self.head = newNode
+            self.tail = newNode
+
+        
+        else:
+            currHead = self.head
+            currHead.prev = newNode
+            newNode.next = currHead
+            self.head = newNode
+        
+        self.length += 1
+        
+    def traverse(self): # time complex O(n)
+        curr = self.head
+        while curr:
+            print(curr)
+            curr = curr.next
+            
+newNode = Node(11)
+print(newNode)
+
+dll = DoublyLinkedList()
+dll.append(10)
+print(dll.tail)
